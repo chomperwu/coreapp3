e976329a980cb76d19570e760d2f327a4894f52d
Administrator <1538477318@qq.com>
2018-09-11 22:37:40 +0800

    9.11-22:35

---
+++ cgi变成/cgi-bin/friendsA.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import cgi
+
+reshtml = '''Content-Type: text/html
+<html><head><title>
+Friends CGI Demo(dynamic)
+</title></head>
+<body><H3>Friends list for: <I>%s<I></H3>
+Your name is: <B>%s</B>
+You have <B>%s</B> friends.
+</body></html>
+'''
+
+form = cgi.FieldStorage()
+who = form['person'].value
+#who = form.getvalue('person')
+howmany = form.getvalue('howmany')
+print reshtml % (who,who,howmany)
+
+
+
+
+
+
+
+
+
+
